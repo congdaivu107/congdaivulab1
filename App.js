164275ae534296a1a5836ea698fbb1b30295b1d5
congdaivu107 <congdaivu107@gmail.com>
2024-09-21 01:56:52 +0700

    update FScreen and S1_a

--- App.js
+++ App.js
@@ -1,20 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import FirstScreen from "./screen/FirstScreen";
+import S1_a from "./screen/S1_a";
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <FirstScreen></FirstScreen>
+    // <S1_a></S1_a>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
